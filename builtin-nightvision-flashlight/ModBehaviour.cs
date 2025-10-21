d7d8544b248689c1447bce3ac60dbd49d6c60836
nuoxi4n <nuo_xian@qq.com>
2025-10-22 03:08:00 +0800

    更新 ModBehaviour.cs 和项目文件，添加夜视和手电筒效果的对话气泡提示

--- builtin-nightvision-flashlight/ModBehaviour.cs
+++ builtin-nightvision-flashlight/ModBehaviour.cs
@@ -1,9 +1,13 @@
-﻿using Duckov.UI;
+﻿using Cysharp.Threading.Tasks;
+using Duckov.UI;
+using Duckov.UI.DialogueBubbles;
 using Duckov.Utilities;
 using ItemStatsSystem;
 using TMPro;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputRemoting;
 
 namespace builtin_nightvision_flashlight
 {
@@ -23,7 +27,7 @@ namespace builtin_nightvision_flashlight
         }
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.B))
             {
                 ToggleFlashlight();
             }
@@ -35,10 +39,9 @@ namespace builtin_nightvision_flashlight
         }
         void OnDestroy()
         {
-            nightVisionActive = false;
             FlashlightActive = false;
+            nightVisionActive = false;
             ApplyFlashlightEffect();
-            ApplyNightVisionEffect();
         }
         void OnEnable()
         {
@@ -53,32 +56,49 @@ namespace builtin_nightvision_flashlight
         }
         private void ToggleNightVision()
         {
-            nightVisionActive = !nightVisionActive;
-            ApplyNightVisionEffect();
-        }
-        private void ApplyFlashlightEffect()
-        {
-            var character = CharacterMainControl.Main;
-            if (character == null) return;
-
-            Debug.Log($"Flash Light {(FlashlightActive ? "Enabled" : "Disabled")}");
-        }
-        private void ApplyNightVisionEffect()
-        {
             var character = CharacterMainControl.Main;
             if (character == null || character.CharacterItem == null) return;
 
             Stat nightVisionStat = character.CharacterItem.GetStat(nightVisionTypeHash);
             if (nightVisionStat != null)
             {
-                nightVisionStat.BaseValue = nightVisionActive ? 1f : 0f;
-                Debug.Log($"Night Vision {(nightVisionActive ? "Enabled" : "Disabled")}");
-            }
+                float currentValue = nightVisionStat.BaseValue;
+                float newValue = (currentValue + 1) % 3;
+                nightVisionStat.BaseValue = newValue;
 
-            if (GameManager.NightVision != null)
-            {
-                GameManager.NightVision.Refresh();
+                Debug.Log($"Night Vision Mode: {newValue}");
+                string message = "";
+                switch (newValue)
+                {
+                    case 0: message = "关闭夜视"; break;
+                    case 1: message = "开启夜视"; break;
+                    case 2: message = "开启热成像"; break;
+                }
+
+                DialogueBubblesManager.Show(message,character.transform,
+                    yOffset: 1f,
+                    needInteraction: false,
+                    skippable: true,
+                    speed: -1f,
+                    duration: 2f).Forget();
+
+                if (GameManager.NightVision != null)
+                {
+                    GameManager.NightVision.Refresh();
+                }
             }
+        }
+        private void ApplyFlashlightEffect()
+        {
+            var character = CharacterMainControl.Main;
+
+            Debug.Log($"Flash Light {(FlashlightActive ? "Enabled" : "Disabled")}");
+            DialogueBubblesManager.Show("暂时没有这个功能", character.transform,
+                    yOffset: 1f,
+                    needInteraction: false,
+                    skippable: true,
+                    speed: -1f,
+                    duration: 2f).Forget();
         }
     }
 }
